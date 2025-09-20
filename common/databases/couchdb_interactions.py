4c45d0a7b6adc77399e1e0e8bdf4eb2a981ac836
Cerys <cerysamberlewis@gmail.com>
2025-09-20 15:41:32 +0100

    upgraded the couchdb library

--- common/databases/couchdb_interactions.py
+++ common/databases/couchdb_interactions.py
@@ -1,11 +1,14 @@
+import logging
 import os
 from typing import Generator
 
-import couchdb
+import couchdb3
 
+from common.utilities.case_utilities import get_cases_collection
 from common.utilities.configuration import get_configuration
 
-COUCHDB_CONNECTION: couchdb.Server
+
+COUCHDB_CONNECTION: couchdb3.Server
 
 
 def get_couchdb_path():
@@ -20,18 +23,17 @@ def get_couchdb_path():
     return f"{protocol}://{username}:{password}@{hostname}:{port}/"
 
 
-def get_couchdb_connection() -> Generator[couchdb.Server, None, None]:
+def get_couchdb_connection() -> Generator[couchdb3.Server, None, None]:
     global COUCHDB_CONNECTION
     configuration = get_configuration()
-
     database_url = get_couchdb_path()
-    COUCHDB_CONNECTION = couchdb.Server(database_url)
+    COUCHDB_CONNECTION = couchdb3.Server(database_url)
 
     for x in ["Cases", "Users"]:
         db_name = configuration.get_string('Databases', 'CouchDB', 'Databases', x)
         try:
             db = COUCHDB_CONNECTION[db_name]
-        except couchdb.ResourceNotFound:
+        except couchdb3.ResourceNotFound:
             db = COUCHDB_CONNECTION.create(db_name)
 
     try:
