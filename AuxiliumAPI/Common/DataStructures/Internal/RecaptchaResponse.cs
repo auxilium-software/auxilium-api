a8c1e51315911a818878d4b28048d91fa41f3117
Cerys <cerysamberlewis@gmail.com>
2025-12-13 17:17:28 +0000

    Create RecaptchaResponse.cs

---
+++ AuxiliumAPI/Common/DataStructures/Internal/RecaptchaResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace AuxiliumAPI.Common.DataStructures.Internal
+{
+    internal class RecaptchaResponse
+    {
+        public bool Success { get; set; }
+        public string? ChallengeTs { get; set; }
+        public string? Hostname { get; set; }
+    }
+}
