7a17f3db78dd06a1ad438a51332531cc7d39fb96
Cerys <cerysamberlewis@gmail.com>
2025-12-22 12:11:05 +0000

    Create AditionalPropertyCreationRequestModel.cs

---
+++ AuxiliumAPI/Models/AdditionalProperty/AditionalPropertyCreationRequestModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace AuxiliumAPI.Models.AdditionalProperty
+{
+    public class AdditionalPropertyCreationRequestModel
+    {
+        [JsonPropertyName("content")]
+        public required string Content { get; set; }
+
+
+        [JsonPropertyName("display_name")]
+        public string? DisplayName { get; set; }
+
+
+        [JsonPropertyName("content_type")]
+        public string? ContentType { get; set; }
+    }
+}
