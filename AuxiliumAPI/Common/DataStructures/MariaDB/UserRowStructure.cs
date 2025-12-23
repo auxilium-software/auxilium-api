f93f02f8b2b91a887897be7205711a2e10ce3cbd
Cerys <cerysamberlewis@gmail.com>
2025-12-23 15:24:59 +0000

    Update UserRowStructure.cs

--- AuxiliumAPI/Common/DataStructures/MariaDB/UserRowStructure.cs
+++ AuxiliumAPI/Common/DataStructures/MariaDB/UserRowStructure.cs
@@ -3,10 +3,11 @@
     public class UserRowStructure
     {
         public required Guid id { get; set; }
-        public DateTime created_at { get; set; }
+        public required DateTime created_at { get; set; }
         public required string email_address { get; set; }
         public required string password_hash { get; set; }
-        public bool allow_login { get; set; } = false;
-        public bool is_admin { get; set; } = false;
+        public required bool allow_login { get; set; } = false;
+        public required bool is_admin { get; set; } = false;
+        public required bool is_case_worker { get; set; } = false;
     }
 }
