4a9754a127af7670ce13c93208ded3809beafdb3
Cerys <cerysamberlewis@gmail.com>
2025-12-24 11:54:30 +0000

    Update UserModel.cs

--- AuxiliumAPI/Common/EntityModels/UserModel.cs
+++ AuxiliumAPI/Common/EntityModels/UserModel.cs
@@ -12,6 +12,15 @@
 
         public string EmailAddress { get; set; }
         public string PasswordHash { get; set; }
+        public string FullName { get; set; }
+        public string FullAddress { get; set; }
+        public string TelephoneNumber { get; set; }
+        public string Gender { get; set; }
+        public DateOnly DateOfBirth { get; set; }
+        public string HowDidYouFindOutAboutOurService { get; set; }
+
+
+        public bool AllowLogin { get; set; }
         public bool IsAdmin { get; set; } = false;
         public bool IsCaseWorker { get; set; } = false;
 
