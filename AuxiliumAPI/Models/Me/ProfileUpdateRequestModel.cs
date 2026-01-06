896c2c57fd5e96b037a2ec372c1ef2f7f75ce72d
Cerys <cerysamberlewis@gmail.com>
2026-01-06 14:39:30 +0000

    Create ProfileUpdateRequestModel.cs

---
+++ AuxiliumAPI/Models/Me/ProfileUpdateRequestModel.cs
@@ -0,0 +1,12 @@
+﻿namespace AuxiliumAPI.Models.Me
+{
+    public class ProfileUpdateRequestModel
+    {
+        public string? FullName { get; set; }
+        public string? FullAddress { get; set; }
+        public string? TelephoneNumber { get; set; }
+        public string? Gender { get; set; }
+        public DateOnly? DateOfBirth { get; set; }
+        public string? HowDidYouFindOutAboutOurService { get; set; }
+    }
+}
