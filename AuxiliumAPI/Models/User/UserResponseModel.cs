3403b1a9c99a84635cbba90b86145e54ccdb6048
Cerys <cerysamberlewis@gmail.com>
2026-01-06 00:02:42 +0000

    added `IsCaseWorker`

--- AuxiliumAPI/Models/User/UserResponseModel.cs
+++ AuxiliumAPI/Models/User/UserResponseModel.cs
@@ -75,5 +75,9 @@ namespace AuxiliumAPI.Models.User
         [Required]
         [JsonPropertyName("isAdmin")]
         public required bool IsAdmin { get; set; } = false;
+
+        [Required]
+        [JsonPropertyName("isCaseWorker")]
+        public required bool IsCaseWorker { get; set; } = false;
     }
 }
