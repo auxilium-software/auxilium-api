af1daa5423c97707fae2ab342025defab347e69b
Cerys <cerysamberlewis@gmail.com>
2026-02-02 17:17:47 +0000

    wemwbs models

---
+++ AuxiliumSoftware.AuxiliumServices.API/Models/WEMWBS/CreateWEMWBSRequestModel.cs
@@ -0,0 +1,63 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AuxiliumSoftware.AuxiliumServices.API.Models.WEMWBS
+{
+    public class CreateWEMWBSRequestModel
+    {
+        [Required]
+        [Range(1, 5)]
+        public int OptimismScore { get; set; }
+
+        [Required]
+        [Range(1, 5)]
+        public int UsefulnessScore { get; set; }
+
+        [Required]
+        [Range(1, 5)]
+        public int RelaxedScore { get; set; }
+
+        [Required]
+        [Range(1, 5)]
+        public int InterestedInPeopleScore { get; set; }
+
+        [Required]
+        [Range(1, 5)]
+        public int SpareEnergyScore { get; set; }
+
+        [Required]
+        [Range(1, 5)]
+        public int ProblemHandlingScore { get; set; }
+
+        [Required]
+        [Range(1, 5)]
+        public int ClearThoughtScore { get; set; }
+
+        [Required]
+        [Range(1, 5)]
+        public int FeelingGoodSelfScore { get; set; }
+
+        [Required]
+        [Range(1, 5)]
+        public int FeelingCloseToPeopleScore { get; set; }
+
+        [Required]
+        [Range(1, 5)]
+        public int ConfidenceScore { get; set; }
+
+        [Required]
+        [Range(1, 5)]
+        public int MakingUpOwnMindScore { get; set; }
+
+        [Required]
+        [Range(1, 5)]
+        public int FeelingLovedScore { get; set; }
+
+        [Required]
+        [Range(1, 5)]
+        public int InterestedInNewThingsScore { get; set; }
+
+        [Required]
+        [Range(1, 5)]
+        public int FeelingCheerfulScore { get; set; }
+    }
+}
---
+++ AuxiliumSoftware.AuxiliumServices.API/Models/WEMWBS/WEMWBSResponseModel.cs
@@ -0,0 +1,11 @@
+﻿namespace AuxiliumSoftware.AuxiliumServices.API.Models.WEMWBS
+{
+    public class WEMWBSResponseModel
+    {
+        public Guid Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public Guid? CreatedBy { get; set; }
+        public int TotalScore { get; set; }
+        public WEMWBSScoresModel Scores { get; set; } = new();
+    }
+}
