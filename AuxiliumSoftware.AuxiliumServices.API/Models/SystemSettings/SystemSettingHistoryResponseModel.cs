2c6b8b8ca626d272236b6d78b572030a9fd5f628
Cerys <cerysamberlewis@gmail.com>
2026-03-11 17:15:20 +0000

    system setting models

---
+++ AuxiliumSoftware.AuxiliumServices.API/Models/SystemSettings/SystemSettingHistoryResponseModel.cs
@@ -0,0 +1,14 @@
+﻿using AuxiliumSoftware.AuxiliumServices.Common.EntityFramework.Enumerators;
+
+namespace AuxiliumSoftware.AuxiliumServices.API.Models.SystemSettings
+{
+    public class SystemSettingHistoryResponseModel
+    {
+        public required string Value { get; set; }
+        public required SystemSettingValueTypeEnum ValueType { get; set; }
+        public required DateTime ModifiedAt { get; set; }
+        public Guid? ModifiedBy { get; set; }
+        public string? ModifiedByName { get; set; }
+        public required string ReasonForModification { get; set; }
+    }
+}
---
+++ AuxiliumSoftware.AuxiliumServices.API/Models/SystemSettings/SystemSettingItemResponseModel.cs
@@ -0,0 +1,16 @@
+﻿namespace AuxiliumSoftware.AuxiliumServices.API.Models.SystemSettings
+{
+    public class SystemSettingItemResponseModel
+    {
+        public string Key { get; set; } = string.Empty;
+        public string JsonKey { get; set; } = string.Empty;
+        public object? Value { get; set; }
+        public string ValueType { get; set; } = string.Empty;
+        public object? DefaultValue { get; set; }
+        public bool IsUsingDefault { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public string Recommendation { get; set; } = string.Empty;
+        public DateTime? LastModifiedAt { get; set; }
+        public Guid? LastModifiedBy { get; set; }
+    }
+}
