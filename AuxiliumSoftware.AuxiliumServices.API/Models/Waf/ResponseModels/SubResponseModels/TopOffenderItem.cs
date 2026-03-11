1f869b86ee524d611a92fdeffc53b123dad3842b
Cerys <cerysamberlewis@gmail.com>
2026-03-11 17:15:13 +0000

    Update TopOffenderItem.cs

--- AuxiliumSoftware.AuxiliumServices.API/Models/Waf/ResponseModels/SubResponseModels/TopOffenderItem.cs
+++ AuxiliumSoftware.AuxiliumServices.API/Models/Waf/ResponseModels/SubResponseModels/TopOffenderItem.cs
@@ -6,6 +6,6 @@
         public required int FailedAttempts { get; set; }
         public required int DistinctUsersTargeted { get; set; }
         public required DateTime LastAttempt { get; set; }
-        // public required bool IsCurrentlyBlocked { get; set; }
+        public required bool IsCurrentlyBlocked { get; set; }
     }
 }
