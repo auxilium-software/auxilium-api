f973b1951b8792211231ef7f7a821e4c0f9f30a7
Cerys <cerysamberlewis@gmail.com>
2026-03-11 17:15:11 +0000

    Update TopTargetedUserItem.cs

--- AuxiliumSoftware.AuxiliumServices.API/Models/Waf/ResponseModels/SubResponseModels/TopTargetedUserItem.cs
+++ AuxiliumSoftware.AuxiliumServices.API/Models/Waf/ResponseModels/SubResponseModels/TopTargetedUserItem.cs
@@ -3,11 +3,9 @@
     public class TopTargetedUserItem
     {
         public required Guid UserId { get; set; }
-        public required string? Email { get; set; }
-        public required string? FullName { get; set; }
         public required int FailedAttempts { get; set; }
         public required int DistinctIpAddresses { get; set; }
         public required DateTime LastAttempt { get; set; }
-        // public required bool IsCurrentlyLockedOut { get; set; }
+        public required bool IsCurrentlyLockedOut { get; set; }
     }
 }
