f825fbfe3bf9fd32cdf28ff403a7aafc9cc35df6
Cerys <cerysamberlewis@gmail.com>
2026-04-13 08:20:53 +0100

    Create IpAddressJsonConverter.cs

---
+++ AuxiliumSoftware.AuxiliumServices.API/JsonSerialisationConverters/IpAddressJsonConverter.cs
@@ -0,0 +1,20 @@
+﻿using System.Net;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace AuxiliumSoftware.AuxiliumServices.API.JsonSerialisationConverters
+{
+    public class IpAddressJsonConverter : JsonConverter<IPAddress>
+    {
+        public override IPAddress? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            var str = reader.GetString();
+            return str is null ? null : IPAddress.Parse(str);
+        }
+
+        public override void Write(Utf8JsonWriter writer, IPAddress value, JsonSerializerOptions options)
+        {
+            writer.WriteStringValue(value.ToString());
+        }
+    }
+}
