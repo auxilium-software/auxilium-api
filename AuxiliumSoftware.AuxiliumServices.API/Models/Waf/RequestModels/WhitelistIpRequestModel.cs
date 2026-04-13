459e921765016e300080b77f6cde90ed4ef91eb4
Cerys <cerysamberlewis@gmail.com>
2026-04-13 08:07:49 +0100

    Update WhitelistIpRequestModel.cs

--- AuxiliumSoftware.AuxiliumServices.API/Models/Waf/RequestModels/WhitelistIpRequestModel.cs
+++ AuxiliumSoftware.AuxiliumServices.API/Models/Waf/RequestModels/WhitelistIpRequestModel.cs
@@ -1,11 +1,21 @@
-﻿using System.Net;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Text.Json.Serialization;
 
 namespace AuxiliumSoftware.AuxiliumServices.API.Models.Waf.RequestModels
 {
     public class WhitelistIpRequestModel
     {
+        [Required]
+        [JsonPropertyName("ipAddress")]
         public required IPAddress IpAddress { get; set; }
+
+        [Required]
+        [JsonPropertyName("reason")]
         public required string Reason { get; set; }
+
+        [Required]
+        [JsonPropertyName("isPermanent")]
         public required bool IsPermanent { get; set; }
     }
 }
