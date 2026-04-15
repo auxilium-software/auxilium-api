2ac064a738330fc2dce0e6daa124b112254f195b
Cerys <cerysamberlewis@gmail.com>
2026-04-15 20:35:56 +0100

    Create AdminCreateUserRequestModel.cs

---
+++ AuxiliumSoftware.AuxiliumServices.API/Models/User/AdminCreateUserRequestModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace AuxiliumSoftware.AuxiliumServices.API.Models.User
+{
+    public class AdminCreateUserRequestModel
+    {
+        [Required]
+        [EmailAddress]
+        [JsonPropertyName("emailAddress")]
+        public required string EmailAddress { get; set; }
+
+
+        [Required]
+        [JsonPropertyName("fullName")]
+        public string FullName { get; set; } = string.Empty;
+
+
+        [Required]
+        [JsonPropertyName("languagePreference")]
+        public string? LanguagePreference { get; set; }
+    }
+}
