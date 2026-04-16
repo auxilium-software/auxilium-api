bb2df6247c7fe77119883c1dcbfed38925255383
Cerys <cerysamberlewis@gmail.com>
2026-04-16 14:10:46 +0100

    file stuff

---
+++ AuxiliumSoftware.AuxiliumServices.API/Common/Utilities/FileUtilities.cs
@@ -0,0 +1,12 @@
+﻿namespace AuxiliumSoftware.AuxiliumServices.API.Common.Utilities
+{
+    public class FileUtilities
+    {
+        public static bool IsScriptCapable(string ct) =>
+            ct.StartsWith("image/svg", StringComparison.OrdinalIgnoreCase)
+            || ct.StartsWith("text/html", StringComparison.OrdinalIgnoreCase)
+            || ct.StartsWith("application/xhtml", StringComparison.OrdinalIgnoreCase)
+            || ct.StartsWith("text/xml", StringComparison.OrdinalIgnoreCase)
+            || ct.StartsWith("application/xml", StringComparison.OrdinalIgnoreCase);
+    }
+}
