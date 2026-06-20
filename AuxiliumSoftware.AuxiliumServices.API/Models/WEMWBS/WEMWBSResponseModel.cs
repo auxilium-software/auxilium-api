40547d933ebf129d0c2552b54c4f0f4b0f0018ea
Cerys <cerysamberlewis@gmail.com>
2026-06-20 12:00:20 +0100

    Update WEMWBSResponseModel.cs

--- AuxiliumSoftware.AuxiliumServices.API/Models/WEMWBS/WEMWBSResponseModel.cs
+++ AuxiliumSoftware.AuxiliumServices.API/Models/WEMWBS/WEMWBSResponseModel.cs
@@ -5,6 +5,7 @@
         public Guid Id { get; set; }
         public DateTime CreatedAt { get; set; }
         public Guid? CreatedBy { get; set; }
+        public Guid? Subject { get; set; }
         public int TotalScore { get; set; }
         public WEMWBSScoresModel Scores { get; set; } = new();
     }
