1302e66489f740b475824a7764cbc27cb0345da6
Cerys <cerysamberlewis@gmail.com>
2026-06-23 23:07:41 +0100

    metrics work

---
+++ AuxiliumSoftware.AuxiliumServices.API/Middleware/HttpMetricsMiddleware.cs
@@ -0,0 +1,37 @@
+﻿using AuxiliumSoftware.AuxiliumServices.API.Metrics;
+using System.Diagnostics;
+
+namespace AuxiliumSoftware.AuxiliumServices.API.Middleware
+{
+    public sealed class HttpMetricsMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly HttpMetricsAccumulator _acc;
+
+        public HttpMetricsMiddleware(RequestDelegate next, HttpMetricsAccumulator acc)
+        {
+            _next = next;
+            _acc = acc;
+        }
+
+        public async Task Invoke(HttpContext ctx)
+        {
+            var sw = Stopwatch.StartNew();
+            var threw = false;
+            try
+            {
+                await _next(ctx);
+            }
+            catch
+            {
+                threw = true;
+                throw;
+            }
+            finally
+            {
+                sw.Stop();
+                _acc.Record(sw.Elapsed.TotalMilliseconds, ctx.Response.StatusCode, threw);
+            }
+        }
+    }
+}
