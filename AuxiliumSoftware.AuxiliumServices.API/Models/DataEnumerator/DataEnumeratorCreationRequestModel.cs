dce889591badbb003c2a3185f91304c18d74cf4d
Cerys <cerysamberlewis@gmail.com>
2026-08-05 22:16:41 +0100

    Update DataEnumeratorCreationRequestModel.cs

--- AuxiliumSoftware.AuxiliumServices.API/Models/DataEnumerator/DataEnumeratorCreationRequestModel.cs
+++ AuxiliumSoftware.AuxiliumServices.API/Models/DataEnumerator/DataEnumeratorCreationRequestModel.cs
@@ -1,10 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using AuxiliumSoftware.AuxiliumServices.Common.EntityFramework.Enumerators;
+using System.ComponentModel.DataAnnotations;
 using System.Text.Json.Serialization;
 
 namespace AuxiliumSoftware.AuxiliumServices.API.Models.DataEnumerator
 {
     public class DataEnumeratorCreationRequestModel
     {
+        [JsonPropertyName("scope")]
+        public DataEnumeratorScopeEnum Scope { get; set; }
+
+
+
         [Required]
         [JsonPropertyName("name")]
         public required string Name { get; set; }
