46b48b5c491b71923c52fe5f2dfd06dc76668080
Cerys <cerysamberlewis@gmail.com>
2026-08-05 22:16:19 +0100

    Update Constants.cs

--- AuxiliumSoftware.AuxiliumServices.API/Common/Constants.cs
+++ AuxiliumSoftware.AuxiliumServices.API/Common/Constants.cs
@@ -3,5 +3,9 @@
     public class Constants
     {
         public const string DataEnumeratorReferenceContentType = "application/x-auxilium-data-enumumerator-value-id";
+
+
+        public const string CalendarEventCategoryDataEnumeratorName = "/data-enumerator/builtin/calendar-event-category";
+        public const string CalendarEventCategoryDataEnumeratorValueDefaultColourHexValue = "#000000";
     }
 }
